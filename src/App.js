754a891503f8f92ab31e4684fba35d022ba90c1d
Spandan Das Barman <spandandb94@gmail.com>
2022-05-04 21:09:40 +0530

    news component and news item component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import NewsComponent from './components/NewsComponent';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <NewsComponent />
     </div>
   );
 }
---
+++ src/components/NewsComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewsItem from './NewsItem';
+
+export default function NewsComponent() {
+  return (
+    <div>
+      <NewsItem />
+    </div>
+  );
+}
